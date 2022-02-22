88510a87e278a412341ae89bd6465db50cd0a592
Yousef Qandeel <yqandil7@gmail.com>
2022-02-22 11:57:12 +0200

    edit

--- server.js
+++ server.js
@@ -7,22 +7,12 @@ const axios = require("axios");
 const dotenv = require("dotenv");
 dotenv.config();
 const APIKEY = process.env.APIKEY;
-function selectData(title, genre_ids, original_language, original_title, poster_path, video, vote_average, overview, release_date, vote_count, id, adult, backdrop_path, popularity, media_type) {
-    this.title = title;
-    this.genre_ids = genre_ids;
-    this.original_language = original_language;
-    this.original_title = original_title;
-    this.poster_path = poster_path;
-    this.video = video;
-    this.vote_average = vote_average;
-    this.overview = overview;
-    this.release_date = release_date;
-    this.vote_count = vote_count;
+function selectData(id, title, release_date, poster_path, overview) {
     this.id = id;
-    this.adult = adult;
-    this.backdrop_path = backdrop_path;
-    this.popularity = popularity;
-    this.media_type = media_type;
+    this.title = title;
+    this.release_date = release_date
+    this.poster_path = poster_path;
+    this.overview = overview;
 }
 app.get('/', homePageHandler);
 app.get('/favorite', favoritePageHandler);
@@ -31,11 +21,11 @@ app.get('/search', searchPageHandler);
 app.use(errorHandler);
 function homePageHandler(req, res) {
     let result = [];
-    movie.data.forEach((value) => {
-        let oneMovie = new selectData(value.title || "N/A", value.poster_path || "N/A", value.overview || "N/A");
-        result.push(oneMovie);
 
-    });
+    let oneMovie = new selectData(movie.id, movie.title || "N/A", movie.release_date, movie.poster_path || "N/A", movie.overview || "N/A");
+    result.push(oneMovie);
+
+
     return res.status(200).json(result);
 }
 function favoritePageHandler(req, res) {
@@ -61,16 +51,16 @@ function searchPageHandler(req, res) {
     console.log(req);
     let response = axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&language=en-US&query=${search}&page=2`)
         .then(apiResponse => {
-            apiResponse.data.results.map(value => {
-                let oneMovie = new selectData(value.id || "N/A", value.title || "N/A", value.release_date || "N/A", value.poster_path || "N/A", value.overview || "N/A");
-                result.push(oneMovie);
-            });
+
+            let oneMovie = new selectData(value.id || "N/A", value.title || "N/A", value.release_date || "N/A", value.poster_path || "N/A", value.overview || "N/A");
+            result.push(oneMovie);
+
             return res.status(200).json(result);
         }).catch(error => {
             errorHandler(error, req, res);
         });
 }
-app.listen(5500, () => {
+app.listen(5501, () => {
     console.log("Welcome from 5500 port");
 });
 function errorHandler(error, req, res) {
