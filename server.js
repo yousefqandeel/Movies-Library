73389776c83078acb905bc67fd7bbe270b8cb92c
Yousef Qandeel <89803447+yousefqaneel@users.noreply.github.com>
2022-03-06 19:30:07 +0300

    Update server.js
    Edit

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.get('/', homePageHandler);
 app.get('/favorite', favoritePageHandler);
 app.get('/trending', trendingPageHandler);
 app.get('/search', searchPageHandler);
+app.get('/discover', discoverHandler);
+app.get("/upcoming", upcomingPageHandler);
+app.get('*', notFounderrorHandler);
 app.use(errorHandler);
 function homePageHandler(req, res) {
     let result = [];
@@ -34,13 +37,13 @@ function favoritePageHandler(req, res) {
 }
 function trendingPageHandler(req, res) {
     let results = []
-    axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`)
+    axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=37ddc7081e348bf246a42f3be2b3dfd0&language=en-US`)
         .then(getResponse => {
             getResponse.data.results.map(value => {
-                let newMovie = new Movie(value.id, value.title, value.poster_path, value.overview);
+                let newMovie = new selectData(value.id, value.title, value.release_date, value.poster_path, value.overview);
                 results.push(newMovie);
             })
-            return res.status(200).json(results);
+            res.status(200).json(results);
         }).catch(error => {
             errorHandler(error, req, res);
         })
@@ -60,13 +63,49 @@ function searchPageHandler(req, res) {
             errorHandler(error, req, res);
         });
 }
+
+function discoverHandler(req, res) {
+    let results = [];
+
+    axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=37ddc7081e348bf246a42f3be2b3dfd0&language=en-US`)
+        .then(getResponse => {
+            getResponse.data.results.map(value => {
+                let discover = new selectData(value.id || "N/A", value.title || "N/A", value.release_date || "N/A", value.poster_path || "N/A", value.overview || "N/A");
+                results.push(discover);
+            })
+            return res.status(200).json(getResponse.data);
+        }).catch(error => {
+            errorHandler(error, req, res);
+        })
+}
+
+function upcomingPageHandler(req, res) {
+    let upcoming = [];
+    axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=37ddc7081e348bf246a42f3be2b3dfd0&language=en-US&page=1`)
+        .then((value) => {
+            value.data.results.forEach((value) => {
+                value = new selectData(value.id, value.title, value.release_date, value.poster_path, value.overview);
+                upcoming.push(value);
+            });
+            return res.status(200).json(value.data);
+        });
+};
+
+
 app.listen(5501, () => {
     console.log("Welcome from 5500 port");
 });
-function errorHandler(error, req, res) {
+function errorHandler(error, req, res, next) {
     const err = {
         status: 500,
-        message: error
+        message: error.message
     }
     return res.status(500).send(err);
+}
+function notFounderrorHandler(req, res) {
+    const err = {
+        status: 404,
+        message: "Not Found"
+    }
+    res.status(404).send(err);
 }
